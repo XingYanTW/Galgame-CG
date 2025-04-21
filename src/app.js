b9d2a0d9be572d86e7d1e68a78df91e29e120932
xy <paul2487rqww95@gmail.com>
2025-04-21 11:46:46 +0800

    simple main page

--- src/app.js
+++ src/app.js
@@ -5,6 +5,25 @@ const imagesRouter = require('./routes/images');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files (if needed)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the main page
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <head>
+                <title>How to Use</title>
+            </head>
+            <body>
+                <h1>Welcome to Galgame-CG Viewer</h1>
+                <p>To view images, navigate to <code>/images/random</code>.</p>
+                <p>Example: <a href="/images">http://localhost:${PORT}/images/random</a></p>
+            </body>
+        </html>
+    `);
+});
+
 app.use('/images', imagesRouter);
 
 app.listen(PORT, () => {
