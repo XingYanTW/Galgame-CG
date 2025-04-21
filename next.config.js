b5742631da73c8b42758a230aebb2a3d62d37b51
xy <paul2487rqww95@gmail.com>
2025-04-21 12:30:56 +0800

    action fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
-  images: {
-    domains: ['localhost'], // Add your domain if needed
-  },
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+
+module.exports = nextConfig
